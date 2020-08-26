8adcd0ae814b13e8b960da36c1aa042962db9110
Tyler Alsop <tylerjalsop@gmail.com>
2020-08-25 19:48:50 -0600

    Fixed issues where room_graph in adv.py wasn't opening the correct file, and where the import in graph3.py wasn't able to find the correct file.

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -4,13 +4,11 @@ from world import World
 from util3 import Queue
 from graph3 import Graph
 
-
 import random
 from ast import literal_eval
 
 # Load world
 world = World()
-
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
@@ -20,7 +18,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 
@@ -32,8 +30,6 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-
-
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -49,7 +45,6 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
